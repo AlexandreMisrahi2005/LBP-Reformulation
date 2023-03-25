472340c6c9b2ac462e4cefcd41fc9935a1344878
AlexandreMisrahi2005 <alex.misrahi@icloud.com>
2023-03-25 14:38:02 +0100

    output to file + statuses

--- blevp2nlp.py
+++ blevp2nlp.py
@@ -1,34 +1,53 @@
 import sys
 import argparse
 import os
+from contextlib import redirect_stdout
 import numpy as np
 from amplpy import AMPL, Environment
 
 model_path = "blevp2nlp.mod"
 
-def reformulate_LBP(solver='baron',ampl_path='',model_path='',data_path='') -> None:
+class ALGERROR(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
 
+def reformulate_LBP(solver='baron',
+                    ampl_path='', 
+                    data_path='',
+                    model_path='') -> None:
     '''
     Solver has to handle NLPs
-    ampl_path: path to folder containing ampl executable
-    model_path: path to AMPL model
-    data_path: path to AMPL-formatted .dat file
+    ampl_path: Path to folder containing AMPL executable.
+    data_path: Path to the LBP data (AMPL-formatted .dat file). Mandatory argument if not testing.
+    model_path: path to AMPL model (.mod file)
     '''
+
     ampl = AMPL(Environment(ampl_path))
     ampl.option["solver"] = solver
 
     ampl.read(model_path)
     ampl.read_data(data_path)
 
+    # !!! verify UL constraints have no LL var (assert the problem is an LBP)
+
+    if ampl.get_parameters()["lambda"].value() != 2:
+        raise ALGERROR("Cannot reformulate: problem instance does not have 2 levels")
+
     ampl.solve()
-    assert ampl.get_value("solve_result") == "solved"
+    status = ampl.get_value("solve_result")
+    print("SOLVER STATUS:",status)
+    if status == "solved":
+        # !!! better handle directories
+        with open(os.path.join("output",data_path[6:-4]+".sol"), 'w') as f:
+            with redirect_stdout(f):
+                ampl.display("x")
 
-    x = [round(ampl.get_variables()["x"][i].value(),8) for i in range(1,int(ampl.get_parameter("n").value())+1)]
-    y = [round(ampl.get_variables()["y"][i].value(),8) for i in range(1,int(ampl.get_parameter("q").value())+1)]
-    z = [round(ampl.get_variables()["z"][i].value(),8) for i in range(1,int(ampl.get_parameter("p").value())+1)]
-    fx = round(ampl.get_objective("obj").get().value(),8)     # objective value
+        fx = ampl.get_objective("obj").get().value()     # objective value
 
-    return (x,y,z),fx
+        return fx
+    else:
+        return None
+
 
 
 if __name__ == "__main__":
@@ -43,15 +62,27 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.test:
-        print("Running sample tests...")
+        print("#####\n# Running sample tests...\n")
 
         for path in os.listdir('tests'):
             data_path = os.path.join('tests', path)
-            if os.path.isfile(data_path) and data_path[-4:] == ".dat":
+            # if os.path.isfile(data_path) and data_path[-4:] == ".dat":
+            if os.path.isfile(data_path) and data_path.endswith(".dat"):
                 print("Solving problem",data_path[6:-4])
-                (x,y,z),fx = reformulate_LBP(
+                fx = reformulate_LBP(
                     ampl_path=args.ampl_path,
                     model_path=model_path,
                     data_path=data_path
                 )
-                print("\tFound solution:",x,y,z,"with value:",fx,"\n")
+                print("##\n")
+        print("# End sample tests\n#####")
+
+    else:
+        if not os.path.isfile(data_path) or data_path[-4:] != ".dat":
+            raise("Wrong file directory or type.")
+        else:
+            fx = reformulate_LBP(
+                ampl_path=args.ampl_path,
+                model_path=model_path,
+                data_path=args.data_path
+            )
